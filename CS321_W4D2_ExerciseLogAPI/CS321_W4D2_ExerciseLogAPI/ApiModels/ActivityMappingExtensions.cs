06d1f4374b13011d4a04e61b291ea1f61dcfdf49
brance17 <59262625+brance17@users.noreply.github.com>
2020-06-28 18:11:43 -0500

    Initial Commit

---
+++ CS321_W4D2_ExerciseLogAPI/CS321_W4D2_ExerciseLogAPI/ApiModels/ActivityMappingExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CS321_W4D2_ExerciseLogAPI.Core.Models;
+
+namespace CS321_W4D2_ExerciseLogAPI.ApiModels
+{
+    public static class ActivityMappingExtenstions
+    {
+
+        public static ActivityModel ToApiModel(this Activity activity)
+        {
+            return new ActivityModel
+            {
+                Id = activity.Id,
+                Date = activity.Date,
+                ActivityTypeId = activity.ActivityTypeId,
+                ActivityType = activity.ActivityType.Name,
+                Duration = activity.Duration,
+                Distance = activity.Distance,
+                UserId = activity.UserId,
+                User = activity.User.Name,
+                Notes = activity.Notes
+
+                // TODO: fill in property mappings
+                // TODO: the ActivityType property should contain the name of the activity type
+                // TODO: the User property should contain the user's name
+            };
+        }
+
+        public static Activity ToDomainModel(this ActivityModel activityModel)
+        {
+            return new Activity
+            {
+                Id = activityModel.Id,
+                Date = activityModel.Date,
+                ActivityTypeId = activityModel.ActivityTypeId,
+                Duration = activityModel.Duration,
+                Distance = activityModel.Distance,
+                UserId = activityModel.UserId,
+                Notes = activityModel.Notes
+                // TODO: fill in property mappings
+                // TODO: leave User and ActivityType null
+            };
+        }
+
+        public static IEnumerable<ActivityModel> ToApiModels(this IEnumerable<Activity> activities)
+        {
+            return activities.Select(a => a.ToApiModel());
+        }
+
+        public static IEnumerable<Activity> ToDomainModels(this IEnumerable<ActivityModel> activityModels)
+        {
+            return activityModels.Select(a => a.ToDomainModel());
+        }
+    }
+}
---
+++ CS321_W4D2_ExerciseLogAPI/CS321_W4D2_ExerciseLogAPI/Controllers/ActivitiesController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CS321_W4D2_ExerciseLogAPI.ApiModels;
+using CS321_W4D2_ExerciseLogAPI.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CS321_W4D2_ExerciseLogAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private IActivityService _activityService;
+
+        public ActivitiesController(IActivityService activityService)
+        {
+            _activityService = activityService;
+        }
+
+
+        // GET: api/<ActivitiesController>
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var activityModels = _activityService.GetAll().ToApiModels();
+
+            return Ok(activityModels);
+        }
+
+        // GET api/<ActivitiesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var activity = _activityService.Get(id);
+
+            if (activity == null) return NotFound();
+
+            return Ok(activity.ToApiModel());
+        }
+
+        // POST api/<ActivitiesController>
+        [HttpPost]
+        public IActionResult Post([FromBody] ActivityModel newActivity)
+        {
+            try
+            {
+                _activityService.Add(newActivity.ToDomainModel());
+            }
+            catch (System.Exception ex)
+            {
+                ModelState.AddModelError("AddActivity", ex.GetBaseException().Message);
+
+                return BadRequest(ModelState);
+            }
+
+            return CreatedAtAction("Get", new { Id = newActivity.Id }, newActivity);
+        }
+
+        // PUT api/<ActivitiesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] ActivityModel updatedActivity)
+        {
+            var activity = _activityService.Update(updatedActivity.ToDomainModel());
+
+            if (activity == null) return NotFound();
+
+            return Ok(activity.ToApiModel());
+        }
+
+        // DELETE api/<ActivitiesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var activity = _activityService.Get(id);
+
+            if (activity == null) return NotFound();
+
+            _activityService.Remove(activity);
+
+            return NoContent();
+        }
+    }
+}
